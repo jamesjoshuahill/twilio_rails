81ae311a2021d78808d98f297b3e72c9613e09b6
Josh Hill <jamesjoshuahill@gmail.com>
2013-10-25 13:33:44 +0100

    New Rails app with PostgreSQL, RSpec, Capybara, Shoulda, FactoryGirl, Guard and HAML

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwilioRails::Application.config.secret_key_base = '60db2e87652ffef9ce8c51f1ca0ec6fb8bb7dab60d7af830bec3c56d36bd76dea0b12e58c991272c60a300949407c8e57d253f3c8489cdfc17784c585ff73937'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TwilioRails::Application.config.session_store :cookie_store, key: '_twilio_rails_session'
